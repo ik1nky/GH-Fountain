8256a2e54a5cd5140fed4b2f2143c8ce7ab182bd
ik1nky <kinkemam@gmail.com>
2015-02-16 12:51:48 -0500

    Create CtlComment.java

---
+++ Current-Work/CtlComment.java
@@ -0,0 +1,18 @@
+/**************************************************************
+* This class takes an existing ctl file and creates a new file
+* that contains comments stating what each line intends to do 
+* in English. This will be displayed by the choreography 
+* software while using the debug mode. Comments are written 
+* between to parentheses (such as this).
+*
+* This class implements uses GH-Fountain/Choreography_Dev/src/
+* choreography/io/CtlLib.java in order to parse each element of
+* the ctl file.
+*
+***************************************************************/
+
+public class CtlComment
+{
+  
+}
+
